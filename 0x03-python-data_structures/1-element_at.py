d505225ed86531415e9597ad243411ba25e8d66e
kristofaumeh <xtopherumeh@gmail.com>
2023-12-31 10:42:07 +0100

    1-elememt.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    #element = None
+
+    list_len = len(my_list)
+
+    if idx >= list_len or idx < 0:
+        return None
+
+    element = my_list[idx]
+
+    return element
